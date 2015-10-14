2fb42cb24b750324343f291c7e8adda5d9bf537b
Boyan Rabchev <boyan@rabchev.com>
2015-10-14 14:07:46 +0300

    Removed unnecessary dependencies.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name starNovaProto.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the starNovaProto
+ */
+angular.module('starNovaProto')
+    .controller('MainCtrl', function () {
+        this.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+    });
